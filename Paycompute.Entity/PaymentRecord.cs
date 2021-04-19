693fe453a5363723134361b1a40ab72ca4956915
unknown <trungsieunhan24@gmail.com>
2021-04-18 20:58:57 -0700

    add domain model

---
+++ Paycompute.Entity/PaymentRecord.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Paycompute.Entity
+{
+    public class PaymentRecord
+    {
+        public int Id { get; set; }
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        [MaxLength(10)]
+        public string Fullname { get; set; }
+        public string NiNo { get; set; }
+        public DateTime PayDate { get; set; }
+        public string PayMonth { get; set; }
+        [ForeignKey("TaxYear")]
+        public int TaxYearId { get; set; }
+        public TaxYear TaxYear { get; set; }
+        public string TaxCode { get; set; }
+        [Column(TypeName = "money")]
+        public decimal HourlyRate { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal HoursWorked { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal ContractualHours { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal OvertimeHours { get; set; }
+        [Column(TypeName = "money")]
+        public decimal ContractualEarnings { get; set; }
+        [Column(TypeName = "money")]
+        public decimal OvertimeEarnings { get; set; }
+        [Column(TypeName = "money")]
+        public decimal Tax { get; set; }
+        [Column(TypeName = "money")]
+        public decimal NIC { get; set; }
+        [Column(TypeName = "money")]
+        public decimal? UnionFee { get; set; }
+        public Nullable<decimal> SLC { get; set; }
+        [Column(TypeName = "money")]
+        public decimal TotalEarning { get; set; }
+        [Column(TypeName = "money")]
+        public decimal TotalDeduction { get; set; }
+        [Column(TypeName = "money")]
+        public decimal NetPayment { get; set; }
+    }
+}
---
+++ Paycompute.Entity/TaxYear.cs
@@ -0,0 +1,8 @@
+﻿namespace Paycompute.Entity
+{
+    public class TaxYear
+    {
+        public int Id { get; set; }
+        public string YearOfTax { get; set; }
+    }
+}
