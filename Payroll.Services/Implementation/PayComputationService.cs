751875bc080115c7609c6ba9f040a60ec686077e
unknown <trungsieunhan24@gmail.com>
2021-04-28 05:24:54 -0700

    rename source

---
+++ Payroll.Services/Implementation/PayComputationService.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Paycompute.Entity;
+using Paycompute.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paycompute.Services.Implementation
+{
+    public class PayComputationService : IPayComputationService
+    {
+        private decimal contractualEarnings;
+        private readonly ApplicationDbContext _context;
+        public PayComputationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate)
+        {
+            if(hoursWorked < contractualHours)
+            {
+                this.contractualEarnings = hoursWorked + hourlyRate;
+            }
+            else
+            {
+                this.contractualEarnings = contractualHours * hourlyRate;
+            }
+            return contractualEarnings;
+        }
+
+        public Task CreateAsync(PaymentRecord paymentRecord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PaymentRecord> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SelectListItem> GetAllTaxYear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PaymentRecord GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaxYear GetTaxYearById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal NetPay(decimal totalEarnings, decimal totalDeduction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal OvertimeHours(decimal hoursWorked, decimal contractualHours)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal OvertimeRate(decimal hourlyRate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TotalDeduction(decimal tax, decimal nic, decimal studentLoanRepayment, decimal unionFees)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ MVC Web Application  using .NET Core 2.2
 ## References
 - https://www.c-sharpcorner.com/article/managing-data-with-viewmodel-in-asp-net-mvc/
 - https://www.c-sharpcorner.com/UploadFile/3d39b4/Asp-Net-mvc-server-side-validation/
+- https://techmaster.vn/posts/36427/quy-uoc-dat-ten-css-theo-chuan-bem
 - https://www.c-sharpcorner.com/article/asp-net-core-mvc-authentication-and-role-based-authorization-with-asp-net-core/
