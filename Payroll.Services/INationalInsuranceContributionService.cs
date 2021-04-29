e90919fc8ed4f848215b3471860d20b409a93369
unknown <trungsieunhan24@gmail.com>
2021-04-28 22:33:32 -0700

    add NI service

---
+++ Payroll.Services/INationalInsuranceContributionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payroll.Services
+{
+    public interface INationalInsuranceContributionService
+    {
+        decimal NIContribution(decimal totalAmount);
+    }
+}
---
+++ Payroll.Services/Implementation/NationalInsuranceContributionService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payroll.Services.Implementation
+{
+    public class NationalInsuranceContributionService : INationalInsuranceContributionService
+    {        
+        public decimal NIContribution(decimal totalAmount)
+        {
+            decimal NIRate = .0m;
+            decimal NIC;
+            if (totalAmount < 719)
+            {
+                //Lower Earning Limit Rate & below Primary Threshold
+                NIC = 0m;
+            }
+            else if (totalAmount >= 719 && totalAmount <= 4167)
+            {
+                //Between Primary Threshold and Upper Earnings Limit (UEL)
+                NIRate = .12m;
+                NIC = ((totalAmount - 719) * NIRate);
+            }
+            else
+            {
+                //Above Upper Earnings Limit (UEL)
+                NIRate = .02m;
+                NIC = ((4167 - 719) * .12m) + ((totalAmount - 4167) * NIRate);
+            }
+            return NIC;
+        }
+    }
+}
