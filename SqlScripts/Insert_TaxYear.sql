95cd387360f3873c799408670a50b131ee8a0e5d
unknown <trungsieunhan24@gmail.com>
2021-05-05 21:24:18 -0700

    add sql script

---
+++ SqlScripts/Insert_TaxYear.sql
@@ -0,0 +1,26 @@
+USE [PayrollDB]
+GO
+
+INSERT INTO [dbo].[TaxYears]
+           ([YearOfTax])
+     VALUES
+           ('2018/2019');
+INSERT INTO [dbo].[TaxYears]
+           ([YearOfTax])
+     VALUES
+           ('2019/2020');
+INSERT INTO [dbo].[TaxYears]
+           ([YearOfTax])
+     VALUES
+           ('2020/2021');
+INSERT INTO [dbo].[TaxYears]
+           ([YearOfTax])
+     VALUES
+           ('2021/2022');
+INSERT INTO [dbo].[TaxYears]
+           ([YearOfTax])
+     VALUES
+           ('2021/2022');
+GO
+
+
